18e504cfcaa9376f499f659073c524a74eb355d8
c2santos <cristiandossantos.ti@gmail.com>
2019-10-18 09:37:31 -0300

    projeto criado

---
+++ src/main/java/com/acmestore/AcmeStoreApplication.java
@@ -0,0 +1,13 @@
+package com.acmestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcmeStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcmeStoreApplication.class, args);
+	}
+
+}
