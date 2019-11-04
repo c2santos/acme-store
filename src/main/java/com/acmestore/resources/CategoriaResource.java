789530faf4c3aaa72c9ecaaa2ac353b33bade205
c2santos <cristiandossantos.ti@gmail.com>
2019-11-04 17:08:10 -0300

    Testando a classe de dominio - categoria

--- src/main/java/com/acmestore/resources/CategoriaResource.java
+++ src/main/java/com/acmestore/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.acmestore.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.acmestore.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Categorias listadas com sucesso";
+	public List<Categoria> listar() {
+		
+		Categoria cat = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> listCategoria = new ArrayList<>();
+		listCategoria.add(cat);
+		listCategoria.add(cat2);
+		
+		return listCategoria;
 	}
 }
