2088c633513c5b02d83301cf46f8688a097d1641
c2santos <cristiandossantos.ti@gmail.com>
2019-11-04 18:51:14 -0300

    Criando operacao de instanciacao

--- src/main/java/com/acmestore/AcmeStoreApplication.java
+++ src/main/java/com/acmestore/AcmeStoreApplication.java
@@ -1,13 +1,34 @@
 package com.acmestore;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class AcmeStoreApplication {
+import com.acmestore.domain.Categoria;
+import com.acmestore.repository.CategoriaRepository;
 
+@SpringBootApplication
+public class AcmeStoreApplication implements CommandLineRunner{
+
+	@Autowired
+	CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AcmeStoreApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat,cat2));
+	}
+	
+	
 
 }
