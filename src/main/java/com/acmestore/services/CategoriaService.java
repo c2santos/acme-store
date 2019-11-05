45e1dda7becbd889064c205c0d2753252d918b0b
c2santos <cristiandossantos.ti@gmail.com>
2019-11-04 21:43:20 -0300

    ajuste no endpoint /categoria/{id}

--- src/main/java/com/acmestore/services/CategoriaService.java
+++ src/main/java/com/acmestore/services/CategoriaService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.acmestore.domain.Categoria;
+import com.acmestore.exceptions.ObjectNotFoundException;
 import com.acmestore.repository.CategoriaRepository;
 
 @Service
@@ -17,6 +18,7 @@ public class CategoriaService {
 	public Categoria buscar(Integer id) {
 		
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: "+ id + ", Tipo: "+ Categoria.class.getName()));
 	}
 }
