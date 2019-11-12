1b7885acf74d3ff370603593cce5dd81260fe1ce
c2santos <cristiandossantos.ti@gmail.com>
2019-11-12 19:31:04 -0200

    Implementado o delete da categoria, criada tratamento de excecao e crada a classe:
    - DataIntegrityException

--- src/main/java/com/acmestore/services/CategoriaService.java
+++ src/main/java/com/acmestore/services/CategoriaService.java
@@ -3,9 +3,11 @@ package com.acmestore.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.acmestore.domain.Categoria;
+import com.acmestore.exceptions.DataIntegrityException;
 import com.acmestore.exceptions.ObjectNotFoundException;
 import com.acmestore.repository.CategoriaRepository;
 
@@ -32,5 +34,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+			
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possi produtos");
+		}
 	}
 }
